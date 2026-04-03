19db9d9775f6934ab062036908e55a9c74f466f7
Blazehubscript <akunrama715@gmail.com>
2026-04-04 00:00:16 +0700

    Update Minigames.lua

--- Minigames.lua
+++ Minigames.lua
@@ -1,5 +1,5 @@
 task.spawn(function()
-    local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Blazehubscript/Loading-screen/refs/heads/main/Loading.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Blazehubscript/Loading-screen/refs/heads/main/Loading.lua"))()
   end)
   task.wait(0.2)
 
